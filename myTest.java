d242dfca027622d696ec962a46ac06f43b436846
zz <1602510371@qq.com>
2020-10-12 20:27:44 +0800

    第一次添加方法

--- myTest.java
+++ myTest.java
@@ -2,4 +2,10 @@ public class{
   public static void main(String[] args){
     System.out.println("Hello World!");
   }
+
+  //第一次添加方法
+  public static void first(){
+    System.out.println("第一次添加方法");	
+  }
 }
+
